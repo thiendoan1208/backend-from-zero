9c2097a37286f56926f2c6defdd6ef4f8791699f
thiendoan1208 <thienmacro1234@gmail.com>
2025-05-24 20:15:21 -0400

    Static file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
 require("dotenv").config();
+const path = require("path");
 
 const app = express(); // tạo app express
 const port = process.env.PORT || 3000;
@@ -9,6 +9,9 @@ const hostname = process.env.HOST_NAME;
 // config template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// config static file
+app.use(express.static(path.join(__dirname, "public")));
 
 // Khai báo routes
 app.get("/", (req, res) => {
