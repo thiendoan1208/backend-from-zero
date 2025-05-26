5fff47a8f5f7f28836c5f548a645e0a678bac2e0
thiendoan1208 <thienmacro1234@gmail.com>
2025-05-25 20:12:24 -0400

    connect app to database

--- src/routes/web.mjs
+++ src/routes/web.mjs
@@ -4,10 +4,8 @@ import { getHomePage, getABC } from "../controllers/homeController.mjs";
 const router = express.Router();
 
 // Routes
-
 // Home Page
 router.get("/", getHomePage);
-
 router.get("/abc", getABC);
 
 export default router;
--- src/server.mjs
+++ src/server.mjs
@@ -1,12 +1,30 @@
 import express from "express";
-// config
 import configViewEngine from "./configs/viewEngine.mjs";
-// routes
 import webRoutes from "./routes/web.mjs";
+import mysql from "mysql2/promise";
 
 const app = express(); // tạo app express
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOST_NAME;
+
+// connection
+const connection = await mysql.createConnection({
+  host: "localhost",
+  port: 3308,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+// A simple SELECT query
+try {
+  const [results, fields] = await connection.query("SELECT * from Users u;");
+
+  console.log("result: ", results); // results contains rows returned by server
+  console.log("field: ", fields); // fields contains extra meta data about results, if available
+} catch (err) {
+  console.log(err);
+}
 
 // run lên server trên port đã khởi tạo trước đấy
 app.listen(port, hostname, () => {
@@ -18,4 +36,3 @@ configViewEngine(app);
 
 // routes
 app.use("/", webRoutes); // => tham số "/" sẽ cho biết toàn bộ các routes sẽ đứng sau nó. VD: /test/abc
-
